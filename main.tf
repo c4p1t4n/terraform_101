f52172d741d9f734ef266e022adf20976c295574
leo oliveira <leoigornunes@protonmail.com>
2025-01-07 19:10:23 -0300

    Change state terraform to s3 backend

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 terraform {
     required_version = "1.10.3"
+    backend "s3" {
+        bucket = "terraform-states-9723"
+        key = "state.tfstate"
+        region = "us-east-2"
+      
+    }
+    
     required_providers {
         aws = {
             source  = "hashicorp/aws"
             version = "~> 5.82"
         }
     }
+
 }
 provider "aws" {
     region = "us-east-2"
