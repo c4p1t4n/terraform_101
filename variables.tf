b41cd9e03431747ce59d785a16bfd9e8d6d8a0ee
leo oliveira <leoigornunes@protonmail.com>
2025-01-07 20:14:40 -0300

    learning about variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
 variable "bucket_name" {
-    description = "Value of the name for Bucket"
+    description = "The namo for bucket name"
     type = string
-    default = "terraform-bucket-9723"
-  
+    default = "value"
+    validation {
+        condition     = length(var.bucket_name) > 10 
+        error_message = "The bucket_name  must > 10 characters "
+    }
+
 }
---
+++ variablesa.auto.tfvars
@@ -0,0 +1,1 @@
+bucket_name = "ola"
